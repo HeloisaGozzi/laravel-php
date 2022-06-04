44b371e7c9526c0031c0012589b2eebe20b8a29b
Heloisa Gozzi <101293482+HeloisaGozzi@users.noreply.github.com>
2022-06-03 22:54:40 -0300

    projetoLaravel

---
+++ database/migrations/2022_05_25_194039_create_funcionario_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionario', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nomeF'); 
+            $table->string('telF'); 
+            $table->string('emailF'); 
+            $table->string('rgF'); 
+            $table->string('cpfF'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionario');
+    }
+};
---
+++ database/migrations/2022_05_25_212143_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('quantidade'); 
+            $table->string('nomeP'); 
+            $table->string('valor'); 
+            $table->string('descricao'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
